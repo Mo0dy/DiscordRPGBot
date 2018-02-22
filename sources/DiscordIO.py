181847cc32b124f3c09dbbe636b6247b2dda64a1
Thomas Knippenberg <thomasknip27@gmail.com>
2018-02-22 14:36:21 +0100

    Added IO
    title

---
+++ sources/DiscordIO.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+import DiscordRPGBot.Settings as Settings
+
+
+def launch_bot():
+    client.run("NDE2MjE5MTY4OTA4NjQwMjU2.DXBSFg.Dt_vYEiiA6F0YBHX7HQ4TvuDcU0")
+
+
+@client.event
+async def on_ready():
+    print("Logged in as")
+    print(client.user.name)
+    print(client.user.id)
+    print("------")
+
+
+@client.event
+async def on_message(message):
+    if(message.content.startswith(Settings.command_prefix)):
+        print(message.content)
