d36c583f6f0c934229ed902b71b3f3d7906e160a
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2018-02-22 15:10:13 +0100

    added events

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
 import sources.DiscordIO as DiscordIO
 
 DiscordIO.launch_bot()
+
+# stores the messages written between iterations
+events = []
---
+++ sources/Event.py
@@ -0,0 +1,6 @@
+
+
+class Event(object):
+    def __init__(self, owner, content):
+        self.owner = owner
+        self.content = content
