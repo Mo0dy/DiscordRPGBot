3d3430f98f46277ae205fb61fb09d6fdac1181fc
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2018-02-22 16:21:36 +0100

    fixed bug

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import sources.DiscordIO as DiscordIO
 
-DiscordIO.launch_bot()
 
 # stores the messages written between iterations
 events = []
 DiscordIO.pass_event_list(events)
+DiscordIO.launch_bot()
