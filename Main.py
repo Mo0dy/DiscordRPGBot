102804124158c744a2e0f39d0b15726a45857cb2
Mo0dy - Felix Mühlenberend <felix.muehlenberend@gmail.com>
2018-02-23 12:20:05 +0100

    started character class

--- Main.py
+++ Main.py
@@ -1,19 +1,38 @@
 import sources.DiscordIO as DiscordIO
+from sources.Game import Game
+import time
+
+
+# some not game related settings
+debug = True
 
 
 # stores the messages written between iterations
 events = []
 DiscordIO.pass_event_list(events)
 
+# create new game
+game = Game()
 
+last_time = time.time()
 def main_loop():
-    global events
-    printed = False
-    for e in events:
-        print(e)
-        printed = True
-    if printed:
-        print("")
+    global events, last_time
+    # calculate dt
+    curr_time = time.time()
+    dt = curr_time - last_time
+    last_time = curr_time
+
+    if debug:
+        printed = False
+        for e in events:
+            print(e)
+            printed = True
+        if printed:
+            print("")
+
+    # update game
+    game.update(events, dt)
+
     # be careful not to change the reference
     del events[:]
 
---
+++ sources/Character.py
@@ -0,0 +1,6 @@
+
+
+class Character(object):
+    def __init__(self):
+        self.hp = 100
+        self.damage = 10
